505f4a82bde4ff7d159854d624fc4584d8509b2f
name <email>
2023-03-09 16:30:08 -0500

    admin stuff

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner,  Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -28,3 +35,7 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
